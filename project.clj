9fbeb554e6c4b67d32cdc71c52a5bfd25fca9b73
Anthony Gallagher <anthonyghr@gmail.com>
2010-08-08 15:11:14 -0400

    A Simple Temporal Network for Clojure

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject chronos "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0-RC2"]
+                 [org.clojure/clojure-contrib "1.2.0-RC2"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
---
+++ test/chronos/test/core.clj
@@ -0,0 +1,6 @@
+(ns chronos.test.core
+  (:use [chronos.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
