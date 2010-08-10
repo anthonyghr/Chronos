896e8cd6907f7a292d4c5d69ef8228f2275cc0ba
Anthony Gallagher <anthonyghr@gmail.com>
2010-08-09 21:41:46 -0400

    Fully developed the STN data structures model

--- src/chronos/core.clj
+++ src/chronos/core.clj
@@ -17,17 +17,26 @@
 
 ;;Define a function to make an edge
 (defrecord edge
-  [constraint from-node to-node weight])
+  [id
+   from-node to-node
+   weight
+   constraint])
+
+(defvar edge-counter (atom 0)
+  "Keep count of the number of edges created")
 
 (defmacro make-edge
   "Create a graph edge given a map of parameters. The returned edge is simply a map with the edge info"
   [& body]
   `(let [edgemap# (hash-map ~@body)]
      ;;Make the edge
-     (edge. (get edgemap# :constraint nil)
-	    (get edgemap# :from-node nil)
-	    (get edgemap# :to-node nil)
-	    (get edgemap# :weight 0))))
+     (let [edge# (edge. (get edgemap# :id @edge-counter)
+			(get edgemap# :from-node nil)
+			(get edgemap# :to-node nil)
+			(get edgemap# :weight 0)
+			(get edgemap# :constraint nil))]
+       (swap! edge-counter inc)
+       edge#)))
 
 ;;Define a function to make a node
 (defrecord node
@@ -35,25 +44,27 @@
    lb lb-pred lb-potential-preds lb-succs
    ub ub-pred ub-potential-preds ub-succs])
 
-(let [node-id (atom 0)]
-  (defmacro make-node
-    "Create a graph node given a map with parameters."
-    [& body]
-    ;;Make the node
-    `(let [nodemap# (hash-map ~@body)]
-       (let [node (node. (get nodemap# :id @node-id)
-			 (get nodemap# :in-edges #{})
-			 (get nodemap# :out-edges #{})
-			 (get nodemap# :lb beginning-of-time)
-			 (get nodemap# :lb-pred nil)
-			 (get nodemap# :lb-potential-preds #{})
-			 (get nodemap# :lb-succs #{})
-			 (get nodemap# :ub end-of-time)
-			 (get nodemap# :ub-pred nil)
-			 (get nodemap# :ub-potential-preds #{})
-			 (get nodemap# :ub-succs #{}))]
-	 (swap! node-id inc)
-	 node))))
+(defvar node-counter (atom 0)
+  "Keep count of the number of nodes created")
+
+(defmacro make-node
+  "Create a graph node given a map with parameters."
+  [& body]
+  ;;Make the node
+  `(let [nodemap# (hash-map ~@body)]
+     (let [node# (node. (get nodemap# :id @node-counter)
+			(get nodemap# :in-edges #{})
+			(get nodemap# :out-edges #{})
+			(get nodemap# :lb beginning-of-time)
+			(get nodemap# :lb-pred nil)
+			(get nodemap# :lb-potential-preds #{})
+			(get nodemap# :lb-succs #{})
+			(get nodemap# :ub end-of-time)
+			(get nodemap# :ub-pred nil)
+			(get nodemap# :ub-potential-preds #{})
+			(get nodemap# :ub-succs #{}))]
+       (swap! node-counter inc)
+       node#)))
 
 ;;--------------------------begin constraint-----------------------------------
 
@@ -63,34 +74,41 @@
    from-node to-node
    forward-edge backward-edge])
 
-(let [constraint-id (atom 0)]
-  (defmacro make-constraint
-    "Create a temporal constraint given a map with parameters."
-    [& body]
-    ;;Make the node
-    `(let [constraintmap# (hash-map ~@body)]
-       (let [node (node. (get constraintmap# :id @constraint-id)
-			 (get constraintmap# :active? false)
-			 (get constraintmap# :lb nil)
-			 (get constraintmap# :ub nil)
-			 (get constraintmap# :from-node nil)
-			 (get constraintmap# :to-node nil)
-			 (get constraintmap# :forward-edge nil)
-			 (get constraintmap# :backward-edge nil))]
-	 (swap! node-id inc)
-	 node))))
+(defvar constraint-counter (atom 0)
+  "Keep count of the number of constraints created")
+
+(defmacro make-constraint
+  "Create a temporal constraint given a map with parameters."
+  [& body]
+  ;;Make the node
+  `(let [constraintmap# (hash-map ~@body)]
+     (let [constraint# (constraint. (get constraintmap# :id @constraint-counter)
+				    (get constraintmap# :active? false)
+				    (get constraintmap# :lb nil)
+				    (get constraintmap# :ub nil)
+				    (get constraintmap# :from-node nil)
+				    (get constraintmap# :to-node nil)
+				    (get constraintmap# :forward-edge nil)
+				    (get constraintmap# :backward-edge nil))]
+       (swap! constraint-counter inc)
+       constraint#)))
 
 ;;--------------------------begin activity-------------------------------------
 
 (defrecord activity
   [source-node finish-node])
 
+(defvar activity-counter (atom 0)
+  "Keep count of the number of activities created")
+
 (defmacro make-activity
   "Create a temporal activity given a map with parameters."
   [& body]
   `(let [activitymap# (hash-map ~@body)]
-     (activity. (get activitymap# :source-node (make-node))
-		(get activitymap# :finish-node (make-node)))))
+     (let [activity# (activity. (get activitymap# :source-node (make-node))
+				(get activitymap# :finish-node (make-node)))]
+       (swap! activity-counter inc)
+       activity#)))
 
 ;;--------------------------begin stn------------------------------------------
 
