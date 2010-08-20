9ca288c639036e7b3234565172296717dc1c80fe
Anthony Gallagher <anthonyghr@gmail.com>
2010-08-19 20:52:58 -0400

    Updated seq2map function to better use Clojure's built-in functions. Created new defrecord+id macro that adds an auto-incrementing ID field to a record. Converting the STN data types to use this macro

--- src/chronos/core.clj
+++ src/chronos/core.clj
@@ -2,7 +2,8 @@
     #^{:author "Anthony Gallagher",
        :doc "A Simple Temporal Network (STN) implementing the TIME96 Cesta & Oddi algorithm"}
     chronos.core
-  (:use clojure.contrib.except
+  (:use chronos.utils
+	clojure.contrib.except
 	[clojure.contrib.def :only (defvar)]))
 
 ;;Constants
@@ -15,115 +16,47 @@
 (defvar neg-infinity (+ Integer/MIN_VALUE 400000000)
   "The minimum allowed time value")
 
-;;Define a function to make an edge
-(defrecord edge
-  [id
-   from-node to-node
-   weight
+;;--------------------------begin edge----------------------------------------
+
+(defrecord+id edge
+  [from-node to-node
+   (weight 0)
    constraint])
 
-(defvar edge-counter (atom 0)
-  "Keep count of the number of edges created")
+;;--------------------------begin node----------------------------------------
 
-(defmacro make-edge
-  "Create a graph edge given a map of parameters. The returned edge is simply a map with the edge info"
-  [& body]
-  `(let [edgemap# (hash-map ~@body)]
-     ;;Make the edge
-     (let [edge# (edge. (get edgemap# :id @edge-counter)
-			(get edgemap# :from-node nil)
-			(get edgemap# :to-node nil)
-			(get edgemap# :weight 0)
-			(get edgemap# :constraint nil))]
-       (swap! edge-counter inc)
-       edge#)))
-
-;;Define a function to make a node
-(defrecord node
-  [id in-edges out-edges
-   lb lb-pred lb-potential-preds lb-succs
-   ub ub-pred ub-potential-preds ub-succs])
-
-(defvar node-counter (atom 0)
-  "Keep count of the number of nodes created")
-
-(defmacro make-node
-  "Create a graph node given a map with parameters."
-  [& body]
-  ;;Make the node
-  `(let [nodemap# (hash-map ~@body)]
-     (let [node# (node. (get nodemap# :id @node-counter)
-			(get nodemap# :in-edges #{})
-			(get nodemap# :out-edges #{})
-			(get nodemap# :lb beginning-of-time)
-			(get nodemap# :lb-pred nil)
-			(get nodemap# :lb-potential-preds #{})
-			(get nodemap# :lb-succs #{})
-			(get nodemap# :ub end-of-time)
-			(get nodemap# :ub-pred nil)
-			(get nodemap# :ub-potential-preds #{})
-			(get nodemap# :ub-succs #{}))]
-       (swap! node-counter inc)
-       node#)))
+(defrecord+id node
+  [in-edges out-edges
+   (lb beginning-of-time) lb-pred lb-potential-preds lb-succs
+   (ub end-of-time) ub-pred ub-potential-preds ub-succs])
 
 ;;--------------------------begin constraint-----------------------------------
 
-(defrecord constraint
-  [id active?
+(defrecord+id constraint
+  [active?
    lb ub
    from-node to-node
    forward-edge backward-edge])
 
-(defvar constraint-counter (atom 0)
-  "Keep count of the number of constraints created")
-
-(defmacro make-constraint
-  "Create a temporal constraint given a map with parameters."
-  [& body]
-  ;;Make the node
-  `(let [constraintmap# (hash-map ~@body)]
-     (let [constraint# (constraint. (get constraintmap# :id @constraint-counter)
-				    (get constraintmap# :active? false)
-				    (get constraintmap# :lb nil)
-				    (get constraintmap# :ub nil)
-				    (get constraintmap# :from-node nil)
-				    (get constraintmap# :to-node nil)
-				    (get constraintmap# :forward-edge nil)
-				    (get constraintmap# :backward-edge nil))]
-       (swap! constraint-counter inc)
-       constraint#)))
-
 ;;--------------------------begin activity-------------------------------------
 
-(defrecord activity
+(defrecord+id activity
   [source-node finish-node])
-
-(defvar activity-counter (atom 0)
-  "Keep count of the number of activities created")
-
-(defmacro make-activity
-  "Create a temporal activity given a map with parameters."
-  [& body]
-  `(let [activitymap# (hash-map ~@body)]
-     (let [activity# (activity. (get activitymap# :source-node (make-node))
-				(get activitymap# :finish-node (make-node)))]
-       (swap! activity-counter inc)
-       activity#)))
 
 ;;--------------------------begin stn------------------------------------------
 
-(defrecord stn
+(defrecord+ stn
   [propagation-mode
    tz-node
    nodes
    constraints])
 
-(defmacro make-stn
-  "Create the STN"
-  [& body]
-  `(let [stnmap# (hash-map ~@body)]
-     (stn. (get stnmap# :propagation-mode :auto)
-	   (get stnmap# :tz-node (make-node :lb beginning-of-time
-					    :ub beginning-of-time))
-	   (get stnmap# :nodes {})
-	   (get stnmap# :constraints {}))))
+(defn propagate-sssp
+  "Propagate effect of constraints using modified Bellman-Ford algorithm"
+  ([stn]
+     (propagate-sssp stn :earliest)
+     (propagate-sssp stn :latest))
+  ([stn direction]
+     ))
+     
+  
--- src/chronos/utils.clj
+++ src/chronos/utils.clj
@@ -2,20 +2,19 @@
     #^{:author "Anthony Gallagher",
        :doc "A macro to define records together with a factory function that gives them defaults"}
     chronos.utils
-  (:use clojure.contrib.except))
+  (:use clojure.contrib.except
+	[clojure.contrib.def :only (defvar)]))
 
-(defn- seq2map
+(defn seq2map
   "Convert a sequence to a map"
   [key-vals]
-  (loop [dmap {}
-	 key-vals key-vals]
-    (if (empty? key-vals)
-      dmap
-      (let [elem (first key-vals)]
-	(recur (assoc dmap (keyword (first elem)) (second elem))
-	       (rest key-vals))))))
+  (->> key-vals
+       (map (fn [[key val]]
+	      [(keyword key) val]))
+       flatten
+       (apply hash-map)))
 
-(defn- make-instance
+(defn make-instance
   "Make an instance of a class with the given parameters"
   [cname params]
   (eval (conj (list* params) cname 'new)))
@@ -25,9 +24,9 @@
    returns an instance of the record initialized with the default values
    provided as part of the record's slot declarations.  e.g.
    (defrecord+ foo
-     [(a 5) b])
-   (new-foo)
-   => #user.foo{:a 5, :b nil}"
+     [(a 5) b c])
+   (make-foo :b 4)
+   => #user.foo{:a 5, :b 4, :c nil}"
   [name slots & etc]
   (let [slots+ (for [slot slots]
 		 (if (list? slot)
@@ -36,9 +35,12 @@
 	fields (->> slots+ (map first) vec)
         default-map (seq2map slots+)]
     `(do
+       ;;Create the record with the given name and fields
        (defrecord ~name
          ~fields
          ~@etc)
+
+       ;;Define the constructor function
        (defn ~(symbol (str "make-" name))
          ~(str "A factory function returning a new instance of " name
 	       " initialized with the defaults specified in the corresponding defrecord+ form.")
@@ -48,4 +50,30 @@
 			      (let [key# (keyword rawkey#)]
 				(get user-map# key# (~default-map key#))))]
 	    (make-instance ~name record-vals#)))
+       ~name)))
+
+(defmacro defrecord+id
+  "Defines a new record, adding an auto-incrementing ID field to it"
+  [name slots & etc]
+  (let [slots+ (conj slots 'id)]
+    `(do
+       ;;Create the record and constructor
+       (defrecord+ ~name
+	 ~slots+
+	 ~@etc)
+
+       ;;Create the counter variable
+       (defvar ~(symbol (str name "-counter")) (atom 0)
+	 ~(str "Keep count of the number of " name "s created"))
+       
+       ;;Create the specialized constructor
+       (defn ~(symbol (str "make-" name "+id"))
+	 ~(str "A factory function returning a new instance of " name
+	       " initialized with the defaults specified in the corresponding defrecord+ form.")
+	 [& body#]
+	 (let [id# @~(symbol (str name "-counter"))
+	       body+# (conj (list* body#) id# :id)
+	       record# (apply ~(symbol (str "make-" name)) body+#)]	 
+	   (swap! ~(symbol (str name "-counter")) inc)
+	   record#))
        ~name)))
